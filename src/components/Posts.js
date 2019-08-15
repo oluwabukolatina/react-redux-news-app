237479f81ab3bd54e26bdadc730d38f5d7cca7ae
Tina Oluwabukola <oluwabukola.ajishebiyawo@tizeti.com>
2019-08-15 19:21:49 +0100

    setting up ui

---
+++ src/components/Posts.js
@@ -0,0 +1,59 @@
+import React, {Component} from 'react';
+
+class Posts extends Component {
+
+    render() {
+
+        return (
+
+            <div className="row">
+                <div className="col-sm-4">
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, temporibus.</h5>
+                            <hr/>
+                            <img className="card-img-top" src="img src" alt="img desription"/>
+                            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem cum enim fugiat molestias mollitia nihil obcaecati possimus reprehenderit sit unde. Aliquam autem consequatur ipsam nostrum odio pariatur, quis rem tempore!</p>
+                            <a href="#" >Lorem ipsum.</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-sm-4">
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, temporibus.</h5>
+                            <hr/>
+                            <img className="card-img-top" src="img src" alt="img desription"/>
+                            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem cum enim fugiat molestias mollitia nihil obcaecati possimus reprehenderit sit unde. Aliquam autem consequatur ipsam nostrum odio pariatur, quis rem tempore!</p>
+                            <a href="#" >Lorem ipsum.</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-sm-4">
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, temporibus.</h5>
+                            <hr/>
+                            <img className="card-img-top" src="img src" alt="img desription"/>
+                            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem cum enim fugiat molestias mollitia nihil obcaecati possimus reprehenderit sit unde. Aliquam autem consequatur ipsam nostrum odio pariatur, quis rem tempore!</p>
+                            <a href="#" >Lorem ipsum.</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-sm-4">
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, temporibus.</h5>
+                            <hr/>
+                            <img className="card-img-top" src="img src" alt="img desription"/>
+                            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem cum enim fugiat molestias mollitia nihil obcaecati possimus reprehenderit sit unde. Aliquam autem consequatur ipsam nostrum odio pariatur, quis rem tempore!</p>
+                            <a href="#" >Lorem ipsum.</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Posts;
